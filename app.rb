3c6960ff3d1b6d6576548553ad086be4b36150f5
Ece Ozalp <github email address>
2016-08-10 23:49:41 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+  get '/name' do
+    "My name is Ece <3"
+  end
+
+  get '/hometown' do
+      "My hometown is İstanbul <3"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Ta Det Lugnt by Dungen <3"
+  end
+
 end
